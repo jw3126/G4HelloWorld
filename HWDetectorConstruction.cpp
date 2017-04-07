062a07525553a26d669608696c5c46a57dcfa4e9
Jan Weidner <jw3126@gmail.com>
2017-04-07 16:40:23 +0200

    ui

--- HWDetectorConstruction.cpp
+++ HWDetectorConstruction.cpp
@@ -19,7 +19,7 @@ G4VPhysicalVolume* HWDetectorConstruction::Construct() {
     G4Material* mediumPhantom = nist -> FindOrBuildMaterial("G4_WATER");
 
     // logical geometry
-    G4Box* solidWorld = new G4Box("solidWorld", 1*km, 2*m, 3*m);
+    G4Box* solidWorld = new G4Box("solidWorld", 1*m, 2*m, 3*m);
     G4LogicalVolume* logicalWorld = new G4LogicalVolume(solidWorld, mediumWorld, "logicalWorld");
 
     // physical geometry
--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <G4VModularPhysicsList.hh>
 #include <QBBC.hh>
 
+#include <G4VisExecutive.hh>
+#include <G4UImanager.hh>
+#include <G4UIExecutive.hh>
+
 #include "HWActionInitialization.h"
 #include "HWDetectorConstruction.h"
 
 #include "G4RunManager.hh"
 
 
-int main() {
+int main(int argc, char** argv) {
     G4Random::setTheEngine(new CLHEP::RanecuEngine);
 
     G4RunManager* runManager = new G4RunManager;  // what is there a difference between G4RunManager() and G4RunManager?
@@ -21,9 +25,21 @@ int main() {
 
     runManager->SetUserInitialization(new HWActionInitialization());
 
-
     runManager->Initialize();
-    runManager->BeamOn(10);
+
+    // visualization
+    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+
+    G4VisManager* visManager = new G4VisExecutive;
+    visManager->Initialize();
+    G4UImanager* uiManager = G4UImanager::GetUIpointer();
+
+    uiManager->ApplyCommand("/control/execute init.mac");
+
+    ui->SessionStart();
+    delete ui;
+
+//    runManager->BeamOn(10);
 
     delete runManager;
 
